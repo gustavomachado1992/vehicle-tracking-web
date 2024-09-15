b7489c05130f74a45c38f6c396c2f88e9d19a080
Gustavo Machado <gustavo@icarustech.com.br>
2024-09-14 21:30:47 -0300

    add angular material && create formt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,48 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { MatSelectModule} from "@angular/material/select";
+import { CommonModule } from '@angular/common';
+import {MatNativeDateModule} from "@angular/material/core";
+import {MatInputModule} from "@angular/material/input";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatDatepickerModule} from "@angular/material/datepicker";
+import {FormBuilder, FormGroup, ReactiveFormsModule} from "@angular/forms";
+import {MatButton} from "@angular/material/button";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    ReactiveFormsModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatNativeDateModule,
+    CommonModule,
+    RouterOutlet,
+    MatButton
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'vehicle-tracking-web';
+  form: FormGroup;
+  options = [
+    { value: 'option1' },
+    { value: 'option2'},
+    { value: 'option3'}
+  ];
+
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      selectedOption: [null],
+      selectedDate: [null]
+    });
+  }
+
+  onSubmit() {
+    console.log('Form Submitted:', this.form.value);
+  }
+
 }
